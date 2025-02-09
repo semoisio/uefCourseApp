e3f192173619766c7cbc45ea7a7c01922ea607e0
Severi Moisio <semoisio@uef.fi>
2025-02-09 14:46:31 +0200

    navbar fixes

--- courseproject/src/pages/Navbar.js
+++ courseproject/src/pages/Navbar.js
@@ -19,10 +19,10 @@ function NavbarOwn() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className={isActive('/uefCourseApp')}  href="/">Home</Nav.Link>
-              <Nav.Link className={isActive('/history')}  href="/history">History</Nav.Link>
-              <Nav.Link  className={isActive('/requirements')} href="/requirements">Requirements</Nav.Link>
-              <Nav.Link className={isActive('/implementation')} href="/implementation">Implementation</Nav.Link>
+              <Nav.Link className={isActive('/uefCourseApp')}  href="/uefCourseApp">Home</Nav.Link>
+              <Nav.Link className={isActive('/history')}  href="/uefCourseApp/history">History</Nav.Link>
+              <Nav.Link  className={isActive('/requirements')} href="/uefCourseApp/requirements">Requirements</Nav.Link>
+              <Nav.Link className={isActive('/implementation')} href="/uefCourseApp/implementation">Implementation</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
